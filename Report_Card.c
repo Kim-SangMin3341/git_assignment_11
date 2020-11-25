0b9c1e7a6414bf56e20667cbc86bc45fd36627b8
Kim-SangMin3341 <aventador3341@gmail.com>
2020-11-25 10:45:30 -0800

    new Report_card

---
+++ Report_Card.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	int kor = 80;
+	int math = 90;
+	int eng = 90;
+	int sum = 80+90+90;
+
+	printf("korean: %d\n",kor);
+	printf("Math: %d\n",math);
+	printf("English: %d\n",eng);
+}
